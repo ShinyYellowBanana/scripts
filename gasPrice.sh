a70d75bf4f403ca85fab486bb1d25abe6d7f0cd6
Matthew Welch <matthew.steven.welch@gmail.com>
2020-12-23 23:26:24 -0500

    192.168.2.2 checkin

---
+++ gasPrice.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+while(true)
+do
+curl -sA 'Mozilla/5.0 (X11; Linux x86_64; rv:30.0) Gecko/20100101 Firefox/30.0'  https://www.samsclub.com/club/melbourne-fl-sams-club/8141?xid=locator_club-details_tooltip_club-name | sed -n -e 's/^.*Unleaded<\/div><div class=\"sc-gas-price sc-club-gas-prices-price\">//p' | cut -c 1-4
+done
