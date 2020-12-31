ba92552105eeaa4f61d5060aabb79f36b40dd7c6
Matthew Welch <matthew.steven.welch@gmail.com>
2020-12-30 19:39:14 -1000

    openAuto Startup script

---
+++ openAuto.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+var=1
+PID=$!
+
+while(true);do
+	if lsusb | grep -q "LG Electronics, Inc." ;then
+		echo "LG Phone Plugged in. $var"
+		var=$((var+1))
+		if [ $var=="2" ];then
+			sudo /home/pi/openauto/bin/autoapp
+			if lsusb | grep -q "LG Electronics, Inc." ;then
+				#Still connected
+				echo "$var"
+				break
+			else
+				echo "SHUTDOWN"
+				#sudo shutdown now
+				exit
+			fi
+		fi
+		
+	fi
+done
