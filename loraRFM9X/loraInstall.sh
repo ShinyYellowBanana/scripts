1869dc0c07fa707e337c106775638c8eec82693d
ShinyYellowBanana <71844805+ShinyYellowBanana@users.noreply.github.com>
2021-01-04 16:19:53 -0500

    Update loraInstall.sh

--- loraRFM9X/loraInstall.sh
+++ loraRFM9X/loraInstall.sh
@@ -1,1 +1,15 @@
-#!bin/bash
+#!/bin/sh
+
+#Print Commands
+set +x
+set -o xtrace
+
+##Distrobution ID
+DIS_ID1=`cat /etc/os-release | sed -n 's/ID_LIKE=//p'` ##Ex. (debian,)
+DIS_ID2=`lsb_release -i | sed -n 's/Distributor ID:\t//p'` ##Ex. (Raspbian, Ubuntu)
+USER=`whoami`
+
+sudo pip3 install adafruit-circuitpython-ssd1306
+sudo pip3 install adafruit-circuitpython-framebuf
+sudo pip3 install adafruit-circuitpython-rfm9x
+wget -O font5x8.bin https://github.com/adafruit/Adafruit_CircuitPython_framebuf/blob/master/examples/font5x8.bin?raw=true
