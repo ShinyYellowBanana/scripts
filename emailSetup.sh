3f851faceeba5af2a8e7260ad6cc2c06267b4dfa
ShinyYellowBanana <71844805+ShinyYellowBanana@users.noreply.github.com>
2021-01-09 11:01:16 -0500

    Update emailSetup.sh

--- emailSetup.sh
+++ emailSetup.sh
@@ -6,12 +6,13 @@ set -o xtrace
 
 sudo apt install msmtp
 read -p 'Email Address: ' $emailaddy
+echo $emailaddy
 msmtp --configure $emailaddy > msmtprc
 echo "account default : $emailaddy" >> msmtprc
 sed -i 's/passwordeval.*/password XXX/g' msmtprc
 sed -i 's/# -.*/### Just replace XXX with your app password/g' msmtprc
 sed -i 's/#  .*/### and press Ctrl-X to quit and save/g' msmtprc
-sudo cp msmtprc /etc/
+sudo mv msmtprc /etc/
 #APP PASWORD NEEDED FROM GMAIL
 sudo pico /etc/msmtprc
 echo "test message from honeyPi" | msmtp -vvv $emailaddy
