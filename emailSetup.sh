58295d250177e5209691b8ffdd37c8201abfa0a5
Matthew Welch <matthew.steven.welch@gmail.com>
2021-01-24 01:09:42 -0500

    Email setup fix

--- emailSetup.sh
+++ emailSetup.sh
@@ -4,7 +4,7 @@
 set +x
 set -o xtrace
 
-sudo apt install msmtp
+sudo apt install msmtp -y
 read -p 'Email Address(Gmail Only): ' emailaddy
 echo $emailaddy
 read -p 'APP Pasword: ' appPassword
@@ -14,5 +14,5 @@ echo "account default : $emailaddy" >> msmtprc
 sed -i "s/passwordeval.*/password ${appPassword}/g" msmtprc
 sudo mv msmtprc /etc/
 #APP PASWORD NEEDED FROM GMAIL
-sudo pico /etc/msmtprc
+###########################################sudo pico /etc/msmtprc
 echo -e "Subject: Setup Complete\r\n\r\nThis is a test mail to confirm the completion of email setup." | msmtp $emailaddy
