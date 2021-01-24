f544100256a94dac2d46d5db221fe5e3f9af71c3
ShinyYellowBanana <71844805+ShinyYellowBanana@users.noreply.github.com>
2021-01-23 23:36:19 -0500

    Update wifiInstall.sh

--- wifiInstall.sh
+++ wifiInstall.sh
@@ -15,7 +15,7 @@ LATEST_BUILD=`git rev-list --count origin/HEAD`
 BRANCH=`git rev-parse --abbrev-ref HEAD` ##Branch Name
 HOSTNAME=`hostname`
 
-if [CURRENT_BUILD != LATEST_BUILD]
+if [CURRENT_BUILD != LATEST_BUILD];then
 	git pull
 	exit
 fi
