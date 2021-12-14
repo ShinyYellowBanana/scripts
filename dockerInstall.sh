ec09c0cea42b2ac48f92a0d71edf4dd90b7436b6
Matthew Welch <matthew.steven.welch@gmail.com>
2021-12-13 21:54:54 -0500

    Fix Docker install

--- dockerInstall.sh
+++ dockerInstall.sh
@@ -4,8 +4,7 @@ sudo apt-get install apt-transport-https ca-certificates software-properties-com
 curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
 sudo usermod -aG docker pi
 sudo curl https://download.docker.com/linux/raspbian/gpg
-echo "deb https://download.docker.com/linux/raspbian/ stretch stable" >> /etc/apt/sources.list
-sudo apt-get update
+sudo echo "deb https://download.docker.com/linux/raspbian/ stretch stable" >> sudo  /etc/apt/sources.list sudo apt-get update
 sudo apt-get upgrade -y
 systemctl start docker.service
 docker info
