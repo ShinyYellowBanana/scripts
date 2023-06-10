95e28d3fcd480bd7a2521852092cd05c89a1a256
Matthew Welch <matthew.steven.welch@gmail.com>
2023-06-10 01:11:00 +0000

    .

--- ups_tool.sh
+++ ups_tool.sh
@@ -16,40 +16,37 @@ usage() {
     echo "  -t     Seconds estimated left (1620)"
     exit 1
 }
-
-# Parse command line options
-while getopts "avpom" opt; do
-    case "$opt" in
-        a)
-            about="2"
-            command="snmpwalk -v2c -c homelab 192.168.2.4 .1.3.6.1.4.1.8072.1.3.2.4.1.2 | grep -i string | awk -F'\"' '{print \$2}' | sed -n '2p'"
-            ;;
-        v)
-            volts="3"
-            command="snmpwalk -v2c -c homelab 192.168.2.4 .1.3.6.1.4.1.8072.1.3.2.4.1.2 | grep -i string | awk -F'\"' '{print \$2}' | sed -n '3p' | bc -l" #Save as a float
-            ;;
-        p)
-            percent="6"
-            command="snmpwalk -v2c -c homelab 192.168.2.4 .1.3.6.1.4.1.8072.1.3.2.4.1.2 | grep -i string | awk -F'\"' '{print \$2}' | sed -n '6p' | awk '{print \$0+0}'" #Save as an int
-            ;;
-        o)
-            online="5"
-            command="snmpwalk -v2c -c homelab 192.168.2.4 .1.3.6.1.4.1.8072.1.3.2.4.1.2 | grep -i string | awk -F'\"' '{print \$2}' | sed -n '5p'"
-            ;;
-        m)
-            time="7"
-            command="snmpwalk -v2c -c homelab 192.168.2.4 .1.3.6.1.4.1.8072.1.3.2.4.1.2 | grep -i string | awk -F'\"' '{print \$2}' | sed -n '7p' | awk '{print \$0+0}'" #Save as an int
-            ;;
-        *)
-            usage
-            ;;
-    esac
-done
-
-echo "$command"
-
 # Ping the address to check if it is reachable
 if ping -c 1 -W 1 "$address" &> /dev/null; then
+    # Parse command line options
+    while getopts "avpom" opt; do
+        case "$opt" in
+            a)
+                about="2"
+                command="snmpwalk -v2c -c homelab 192.168.2.4 .1.3.6.1.4.1.8072.1.3.2.4.1.2 | grep -i string | awk -F'\"' '{print \$2}' | sed -n '2p'"
+                ;;
+            v)
+                volts="3"
+                command="snmpwalk -v2c -c homelab 192.168.2.4 .1.3.6.1.4.1.8072.1.3.2.4.1.2 | grep -i string | awk -F'\"' '{print \$2}' | sed -n '3p' | bc -l" #Save as a float
+                ;;
+            p)
+                percent="6"
+                command="snmpwalk -v2c -c homelab 192.168.2.4 .1.3.6.1.4.1.8072.1.3.2.4.1.2 | grep -i string | awk -F'\"' '{print \$2}' | sed -n '6p' | awk '{print \$0+0}'" #Save as an int
+                ;;
+            o)
+                online="5"
+                command="snmpwalk -v2c -c homelab 192.168.2.4 .1.3.6.1.4.1.8072.1.3.2.4.1.2 | grep -i string | awk -F'\"' '{print \$2}' | sed -n '5p'"
+                ;;
+            m)
+                time="7"
+                command="snmpwalk -v2c -c homelab 192.168.2.4 .1.3.6.1.4.1.8072.1.3.2.4.1.2 | grep -i string | awk -F'\"' '{print \$2}' | sed -n '7p' | awk '{print \$0+0}'" #Save as an int
+                ;;
+            *)
+                usage
+                ;;
+        esac
+    done
+  
     # If ping is successful, execute the command over SSH and capture the response
     response=$(ssh "$user"@"$address" $command)
     echo "$response"
